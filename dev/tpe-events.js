b82910df15427b7dcb842351f655edd923a60d53
avondero <romain@avonde.net>
2010-07-07 12:12:33 +0200

    first commit

---
+++ dev/tpe-events.js
@@ -0,0 +1,41 @@
+﻿TPE.Events = {};
+
+$w('click mouseover mousedown mouseup mousemove mouseout keydown keyup keypress mousewheel scroll').each(
+	function(events){
+		TPE.Events[events] = function(element, fct){
+			element = $(element);
+			element.observe(events, fct);
+			return element;		
+		};
+	}
+);
+
+Object.extend(Event, {
+	KEY_SHIFT: 16,
+	
+	/*
+ * Orginal: http://adomas.org/javascript-mouse-wheel/
+ * prototype extension by "Frank Monnerjahn" <themonnie@gmail.com>
+ */
+	
+	wheel:function (event){
+		var delta = 0;
+		if (!event) event = window.event;
+		if (event.wheelDelta) {
+			delta = event.wheelDelta/120; 
+			if (window.opera) delta = -delta;
+		} else if (event.detail) { delta = -event.detail/3;	}
+		return Math.round(delta); //Safari Round
+	}
+});
+
+//###########################################################################################//
+
+(function(){	
+	if (TPE.Utils.paramScript === "true") {
+		Element.addMethods(TPE.Events);
+		Element.addMethods('input', TPE.Events);
+	}
+})();
+
+
